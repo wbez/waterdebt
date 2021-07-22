6f5af6afa710d9dc3a22ce65666114e794f818fe
mattkiefer <matthewlkiefer@gmail.com>
2021-07-21 20:18:36 -0500

    fixed address partitioning

--- debt/migrations/0002_load_debt_cases.py
+++ debt/migrations/0002_load_debt_cases.py
@@ -4,16 +4,17 @@ from django.db import transaction
 from debt.models import Property, Debt, Case
 from water.settings import BASE_DIR
 import agate, agateexcel
-
+import re
 
 
 ### START CONFIG ###
 data_dir = str(BASE_DIR) + '/data/'
 metered_unmetered_path = data_dir + 'Metered.NonMeteredproperties.xlsx'
-debt_data_path = data_dir + 'Administrative hearings data.xlsx'
-admin_data_path = data_dir + 'FOIA Response - Utility Debt Law Firms WBEZ MIZ.xlsx'
+debt_data_path = data_dir + 'FOIA Response - Utility Debt Law Firms WBEZ MIZ.xlsx'
+admin_data_path = data_dir + 'Administrative hearings data.xlsx'
 # not sure about this dataset ... no addresses
 delinquent_data_path = data_dir + 'Delinquent Accounts 2010 to Present WBEZ MIZ.xlsx'
+street_suffixes = ['AVE','BLVD','CT','DR','PL','RD','ST']
 ### END CONFIG ###
 
 
@@ -27,6 +28,114 @@ def load_data(apps,schema_editor):
     load_case()
 
 
+def partition_address(address):
+    """
+    take a complete address
+    and break it into parts via inference
+    """
+    print(address)
+    split_address = address.split()
+    numerical = find_numerical_address(split_address)
+    directional = split_address[1] if split_address[1] in ['N','S','E','W'] else None
+    street_suffix = find_street_suffix(split_address)
+    zipcode = find_zipcode(split_address)
+    try:
+        unit, unit_before = find_unit_no(split_address,zipcode)
+    except Exception as e:
+        print(e)
+        import ipdb; ipdb.set_trace()
+    # figure this out last by process of elimination
+    street_name = find_street_name(split_address, numerical, directional, street_suffix, unit, unit_before)
+    return {
+            'numerical': numerical,
+            'directional': directional,
+            'street_name': street_name,
+            'street_suffix': street_suffix,
+            'unit': unit,
+            'zipcode': zipcode,
+           }
+
+
+def find_numerical_address(split_address):
+    """
+    coerce first split element to int
+    or else give up and return None
+    """
+    try:
+        return split_address[0] if int(split_address[0]) else None
+    except:
+        return None
+
+
+def find_street_name(split_address, numerical, directional, suffix, unit, unit_before):
+    """
+    the hardest part.
+    start from the earliest possible index and bump it depending on 
+    how many things precede it
+    """
+    street_name_starting_position = 0
+    street_name_ending_position = -1 
+
+    if numerical:
+        street_name_starting_position += 1
+    if unit_before:
+        street_name_starting_position += 1
+    if directional:
+        street_name_starting_position += 1
+
+    if suffix: # easy way to see where the street name ends
+        street_name_ending_position = split_address.index(suffix)
+    elif unit and not unit_before: # if we don't have a suffix, check for a unit after the street name
+        street_name_ending_position = split_address.index('UNIT')
+    return ' '.join(split_address[street_name_starting_position:street_name_ending_position])
+ 
+
+def find_street_suffix(split_address):
+    """ 
+    see if hardcoded suffixes match any address parts
+    and return them
+    """
+    # assumes all caps
+    for suffix in street_suffixes:
+        if suffix in split_address:
+            return suffix    
+   
+
+def find_unit_no(split_address,zipcode):
+    """
+    if there's a "unit"
+    use the index to return that substring
+    plus everything up until the last element [-1] (zipcode)
+
+    ... alternatively, return the second numerical address part ...
+    
+    and specify if the unit number is before the street name (True)
+    """
+    # if a zipcode is present, exclude it from the unit segment
+    unit_end_index = -1 if zipcode else None
+    try:
+        # assumes all caps
+        if 'UNIT' in split_address:
+            unit_start_index = split_address.index('UNIT')
+            return ' '.join(split_address[unit_start_index:unit_end_index]), False
+        # sometimes the unit number follows numerical address
+        elif int(split_address[1]) or int(split_address) == 0: # or len < 5 and not in suffixes
+            return split_address[1], True
+            #TODO resolve situations where unit has alpha characters, e.g. 221 B Baker St
+    except: # need to catch exception when 2nd check fails validation
+        return None, None
+
+def find_zipcode(split_address):
+    """
+    takes last item from split address
+    and checks if its zip or zip+4
+    and if it checks out as int (minus the - sign in zip+4)
+    """
+    try:
+        return split_address[-1] if len(split_address[-1]) in (5,10) and int(split_address[-1].replace('-','')) else None
+    except:
+        return None
+
 def load_property():
     """
     load metered/unmetered data
@@ -37,19 +146,37 @@ def load_property():
     unmetered = agate.Table.from_xlsx(metered_unmetered_path,sheet='NonMetered')
     # metered properties are listed in their own sheet
     for meter in metered:
-        try:
-            with transaction.atomic(): # this wrapper lets you back out of a save in case of a dupe
-                meter_dict = row_to_dict(meter,metered.column_names)
-                prop = Property.objects.create(full_address=meter_dict['Address'],metered=True)
-                prop.save()
-        except Exception as e:
-            print(e)
+        meter_dict = row_to_dict(meter,metered.column_names)
+        create_prop(full_address=meter_dict['Address'],metered=True) 
 
     # unmetered properties listed in the other sheet, different columns
     for unmeter in unmetered:
         unmeter_dict = row_to_dict(unmeter,unmetered.column_names)
-        prop = Property.objects.create(full_address=meter_dict['address nonMetered'],metered=False)
-        prop.save()
+        create_prop(full_address=unmeter_dict['address nonMetered'],metered=False)
+
+
+def create_prop(full_address,metered):
+    """
+    create a property record from a full address
+    """
+    partitioned_address = partition_address(full_address)
+    # the bare minimum needed to create a property
+    if partitioned_address['numerical'] and partitioned_address['street_name']:
+        try:
+            with transaction.atomic(): # keep looping after IntegrityError (unique) 
+                prop = Property.objects.create(
+                        full_address=full_address,
+                        numeric_address = partitioned_address['numerical'],
+                        street_dir = partitioned_address['directional'],
+                        street_name = partitioned_address['street_name'],
+                        street_suffix = partitioned_address['street_suffix'],
+                        unit = partitioned_address['unit'],
+                        zipcode = partitioned_address['zipcode'],
+                        metered = metered)
+                prop.save()
+                print(prop.__dict__)
+        except Exception as e:
+            print(e)
 
 
 def load_debt():
@@ -68,7 +195,19 @@ def load_debt():
     for dd in debt_data:
         dd_dict = row_to_dict(dd, debt_data_2015.column_names)
         # the hard part here is water debt and finance dept addresses are prob structured differently
-        prop = Property.objects.get(full_address=dd_dict['PREMADDRESS'])
+        partitioned_address = partition_address(dd_dict['PREMADDRESS'])
+        print(partitioned_address)
+        props = Property.objects.filter(
+                numeric_address = partitioned_address['numerical'],
+                street_dir = partitioned_address['directional'],
+                street_name = partitioned_address['street_name'],
+                )
+        # per Maria, unit doesn't matter. do street suffixes?
+        if len(list(props)) == 1:
+            # need a scalar to be sure
+            prop = props[0]
+        else:
+            prop = None
         debt = Debt.objects.create(
                 prop = prop,
                 full_address = dd_dict['PREMADDRESS'],
@@ -83,6 +222,7 @@ def load_debt():
 				# no_occurrences?
                 )
         debt.save()
+        print(debt.__dict__)
 
 
 def load_case():
--- debt/models.py
+++ debt/models.py
@@ -3,7 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Property(models.Model):
-    full_address = models.CharField(max_length=100, unique=True)
+    full_address = models.CharField(max_length=200, unique=True)
+    # address parts
+    numeric_address = models.IntegerField()
+    street_dir = models.CharField(max_length=1, null=True)
+    street_name = models.CharField(max_length=50)
+    street_suffix = models.CharField(max_length=10, null=True)
+    unit = models.CharField(max_length=30, null=True)
+    zipcode = models.CharField(max_length=10,null=True)
+    # metered or unmetered water service
     metered = models.BooleanField(null=True)
 
 
