28a35689d6087899ee09d02978ee47099de97dd7
mattkiefer <matthewlkiefer@gmail.com>
2021-10-29 15:37:18 -0500

    revising methodology for total debt

--- reporting/analysis.py
+++ reporting/analysis.py
@@ -19,6 +19,59 @@ reports_dir = str(BASE_DIR) + '/reporting/reports/'
 ### END CONFIG ###
 
 
+###############
+#             #
+# BIG NUMBERS #
+#             # 
+###############
+
+
+def universe_of_accounts():
+    """
+    the big number of this story
+    per AL, let's be conservative and go with the smaller number
+    (current balance, not the itemized total)
+    but only include active accounts, balances > 0 (not negative debt)
+    """
+    return Account.objects.filter(
+            balance__gt=0,
+            status='A'
+            )
+
+
+def universe_of_debt():
+    """
+    the big number for the story
+    current balance for each account in the universe
+    """
+    accounts = universe_of_accounts()
+    total_debt = sum([
+        x.balance
+        for x in accounts])
+    summed_debt = sum([
+                    sum([
+                        x.water_balance,
+                        x.sewer_balance,
+                        x.tax_balance,
+                        x.penalty_balance,
+                        x.garbage_balance,
+                        x.other_balance
+                    ]) 
+                    for x in accounts])
+    if total_debt < summed_debt:
+        return total_debt
+    else:
+        return summed_debt
+
+
+###################
+#                 #
+# END BIG NUMBERS #
+#                 # 
+###################
+
+
+
 def debt_by_unmetered():
     accounts = Account.objects.all()
     nonmetered_accounts = Account.objects.filter(metered=False)
@@ -30,32 +83,52 @@ def debt_by_zipcode():
     return zip, # debts, debt $, racial majority
     """
     # most/all chicago zips start with 606
-    chi_zips = ZipCode.objects.filter(five_digit__startswith='606')
+    #chi_zips = ZipCode.objects.filter(five_digit__startswith='606')
+    chi_zips = ZipCode.objects.all()
     rows = []
     for chi_zip in chi_zips:
         # get all active accounts for this zip
-        zip_debts = Account.objects.filter(zipcode=chi_zip.five_digit,status='A')
+        zip_debts = Account.objects.filter(zipcode=chi_zip.five_digit,status='A',balance__gt=0)
+        zip_props = Property.objects.filter(zipcode=chi_zip.five_digit)
         # get number and total amt of debts
         row = {
                 'zipcode': chi_zip.five_digit,
                 'racial_maj': chi_zip.majority_race,
+                'total_pop': chi_zip.total_pop,
                 'no_debts': len(zip_debts),
                 'total_debt_amt': sum([x.balance for x in zip_debts]),
-                'summed_debt_amt': sum([sum([x.water_balance,x.sewer_balance,x.tax_balance,x.penalty_balance,x.garbage_balance,x.other_balance]    ) for x in zip_debts]),
+                'summed_debt_amt': sum([sum([x.water_balance,x.sewer_balance,x.tax_balance,x.penalty_balance,x.garbage_balance,x.other_balance]) for x in zip_debts]),
                 'water_balance': sum([x.water_balance for x in zip_debts]),
                 'sewer_balance': sum([x.sewer_balance for x in zip_debts]),
                 'tax_balance': sum([x.sewer_balance for x in zip_debts]),
                 'penalty_balance': sum([x.penalty_balance for x in zip_debts]),
                 'garbage_balance': sum([x.garbage_balance for x in zip_debts]),
-                'other_balance': sum([x.other_balance for x in zip_debts])
+                'other_balance': sum([x.other_balance for x in zip_debts]),
+                #'no_metered_homes': len([x for x in zip_props if x.metered]),
+                'no_metered_homes': len([x for x in zip_debts if x.metered]),
+                #'no_unmetered_homes': len([x for x in zip_props if not x.metered]),
+                'no_unmetered_homes': len([x for x in zip_debts if not x.metered]),
+                'total_debt_metered': sum([x.balance for x in zip_debts if x.metered]),
+                'total_debt_unmetered': sum([x.balance for x in zip_debts if not x.metered])
                 }
-        rows.append(row)
+        if row['no_debts']:
+            #row['avg_debt'] = row['total_debt_unmetered']/len(zip_props) if len(zip_props) else None
+            row['avg_debt'] = row['total_debt_unmetered']/len(zip_debts) if len(zip_debts) else None
+            row['avg_metered_debt'] = row['total_debt_unmetered']/row['no_metered_homes'] if row['no_metered_homes'] else None
+            row['avg_unmetered_debt'] = row['total_debt_unmetered']/row['no_unmetered_homes'] if row['no_unmetered_homes'] else None
+            rows.append(row)
+
+    sorted_rows = sorted(rows,key = lambda x: x['no_debts'], reverse = True)
     
     # write results
     outfile = open(reports_dir + 'debt_by_zipcode.csv','w')
-    outcsv = csv.DictWriter(outfile,rows[0].keys())
+    outcsv = csv.DictWriter(outfile,sorted_rows[0].keys())
     outcsv.writeheader()
-    outcsv.writerows(rows)
+    try:
+        outcsv.writerows(sorted_rows)
+    except Exception as e:
+        print(e)
+        import ipdb; ipdb.set_trace()
     outfile.close()
 
 
@@ -169,7 +242,64 @@ def vacancies_and_debts(delta=90):
                 row['bad_debt_no'] += debt.bad_debt_no + ' | '
                 row['debt_date'] += str(debt.debt_date) + ' | '
         if debts:
+            # TODO: per household debt
             outcsv.writerow(row)
 
     outfile.close()
 
+
+def account_balances():
+    rows = []
+    for account in Account.objects.all():
+        row = {
+                'db_id': account.id, 
+                'account_address': account.address, 
+                'summed_balance': sum([account.water_balance, account.sewer_balance,account.tax_balance,account.penalty_balance,account.garbage_balance,account.other_balance])
+        }
+        rows.append(row)
+    # write to file
+    outfile = open('reporting/reports/account_balances.csv','w')
+    outcsv = csv.DictWriter(outfile,row.keys())
+    outcsv.writeheader()
+    for row in rows:
+        outcsv.writerow(row)
+    outfile.close()
+   
+
+def pct_debt_from_black_zipcodes(method='total'):
+    mbzs = ZipCode.objects.filter(majority_race='Black')
+
+    # straight up balances
+    if method=='total':
+        total_debt = sum([x.balance for x in Account.objects.filter(status='A')])
+        black_zip_total_debt = 0
+
+        for mbz in mbzs:
+            for acct in Account.objects.filter(zipcode=mbz.five_digit,status='A'):
+                black_zip_total_debt += acct.balance
+
+        return black_zip_total_debt/total_debt
+
+    # only balances over 0$
+    elif method == 'actual':
+        total_actual_debt = sum([x.balance for x in Account.objects.filter(status='A',balance__gt=0)])
+
+        black_zip_total_actual_debt = 0
+
+        for mbz in mbzs:
+            for acct in Account.objects.filter(zipcode=mbz.five_digit,status='A'):
+                if acct.balance > 0:
+                    black_zip_total_actual_debt += acct.balance
+
+        return black_zip_total_actual_debt / total_actual_debt
+
+    # sum up each balance manually
+    elif method == 'summed':
+        summed_debt = sum([sum([x.water_balance, x.sewer_balance, x.tax_balance, x.garbage_balance, x.penalty_balance, x.other_balance]) for x in Account.objects.filter(status='A')])
+
+        black_zip_summed_debt = 0
+        for mbz in mbzs:
+            for acct in Account.objects.filter(zipcode=mbz.five_digit,status='A'):
+                black_zip_summed_debt += sum([acct.water_balance, acct.sewer_balance, acct.tax_balance, acct.garbage_balance, acct.penalty_balance, acct.other_balance])
+
+        return black_zip_summed_debt/summed_debt
